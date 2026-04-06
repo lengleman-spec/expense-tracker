1c997f4a38746aaede1647d4162bd0b30c15481b
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-04-06 14:41:38 -0400

    Create function to load saved transactions and prevent page refresh.

---
+++ script.js
@@ -0,0 +1,34 @@
+const balanceEl = document.getElementById("balance");
+const incomeAmountEl = document.getElementById("income-amount");
+const expenseAmountEl = document.getElementById("expense-amount");
+const transactionListEl = document.getElementById("transaction-list");
+const transactionFormEl = document.getElementById("transaction-form");
+const descriptionEl = document.getElementById("description");
+const amountEl = document.getElementById("amount");
+
+let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+transactionFormEl.addEventListener("submit", addTransaction);
+
+function addTransaction(e) {
+  e.preventDefault();
+
+  // get form values
+
+  const description = descriptionEl.value.trim();
+  const amount = parseFloat(amountEl.value);
+
+  transactions.push({
+    id: Date.now(),
+    description,
+    amount,
+  });
+}
+
+// save to local storage
+localStorage.setItem("transactions", JSON.stringify(transactions));
+
+// update the UI
+updateTransactionList();
+updateSummary();
+transactionFormEl.reset();
