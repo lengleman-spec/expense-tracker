4dec9059dd9cecea0efc613b3d4d279619e58d48
lengleman-spec <191269315+lengleman-spec@users.noreply.github.com>
2026-04-08 13:06:14 -0400

    Add function to update the overall income.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log("script loaded");
+
 const balanceEl = document.getElementById("balance");
 const incomeAmountEl = document.getElementById("income-amount");
 const expenseAmountEl = document.getElementById("expense-amount");
@@ -43,4 +45,40 @@ function updateTransactionList() {
 
     transactionListEl.appendChild(transactionEl);
   });
+}
+
+function createTransactionElement(transaction) {
+  const li = document.createElement("li");
+  li.classList.add("transaction");
+  li.classList.add(transaction.amount > 0 ? "income" : "expense");
+
+  li.innerHTML = `
+  <span>${transaction.description}</span>
+  <span>${transaction.amount}
+    <button class="delete-btn" onclick="removeTransaction(${transaction.id})">X</button>
+  </span>
+  `;
+
+  return li;
+}
+
+function updateSummary() {
+  const balance = transactions.reduce(
+    (acc, transaction) => acc + transaction.amount,
+    0,
+  );
+
+  const income = transactions
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+  const expenses = transactions
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+  //Update UI
+  // Todo fix formatting
+  balanceEl.textContent = balance;
+  incomeAmountEl.textContent = income;
+  expenseAmountEl.textContent = expenses;
 }
